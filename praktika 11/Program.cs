5da17a0999f0af973e176b73c75bd66366ca7f05
Artzze <Artzze@FOXPC>
2023-12-20 11:03:49 +0300

    Add project files.

---
+++ praktika 11/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void BubbleSortOddElements(int[] arr)
+    {
+        int n = arr.Length;
+        // Проходим только по нечетным индексам массива
+        for (int i = 1; i < n - 1; i += 2)
+        {
+            // Внутренний цикл для пузырьковой сортировки
+            for (int j = 1; j < n - i - 1; j += 2)
+            {
+                // Сравниваем и меняем местами, если нужно
+                if (arr[j] > arr[j + 2])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 2];
+                    arr[j + 2] = temp;
+                }
+            }
+        }
+    }
+
+    static void Main()
+    {
+        int[] array = { 5, 1, 4, 2, 8, 0 };
+
+        Console.WriteLine("Original array:");
+        foreach (int element in array)
+        {
+            Console.Write(element + " ");
+        }
+
+        BubbleSortOddElements(array);
+
+        Console.WriteLine("\nArray after sorting odd positions:");
+        foreach (int element in array)
+        {
+            Console.Write(element + " ");
+        }
+    }
+}
